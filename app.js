65b4355607d04243f87d6f59682977405830d0f7
Ryan Khatib <ryan.khatib@gmail.com>
2018-05-07 14:34:02 -0700

    completed functions

---
+++ app.js
@@ -0,0 +1,38 @@
+'use strict';
+/* exported showSum, showDifference, showRemainder, remainder, sumCalc, subCalc, divCalc */
+
+// Addition Calc 
+function showSum() {
+    const x = parseInt(document.getElementById('addend-1').value);
+    const y = parseInt(document.getElementById('addend-2').value);
+    const result = sumCalc(x, y);
+    document.getElementById('sum').textContent = result.value;
+    document.getElementById('sum-description').textContent = result.description;
+}
+
+// Subtraction Calc 
+function showDifference() {
+    const x = parseInt(document.getElementById('minuend').value);
+    const y = parseInt(document.getElementById('subtrahend').value);
+    const result = subCalc(x, y);
+    document.getElementById('difference').textContent = result.value;
+    document.getElementById('sub-description').textContent = result.description;
+}
+
+// Multiplication Calc 
+function showProduct() {
+    const x = parseInt(document.getElementById('multiplicand').value);
+    const y = parseInt(document.getElementById('multiplier').value);
+    const result = multCalc(x, y);
+    document.getElementById('product').textContent = result.value;
+    document.getElementById('product-description').textContent = result.description;
+}
+
+// Division Calc 
+function showRemainder() {
+    const x = parseInt(document.getElementById('dividend').value);
+    const y = parseInt(document.getElementById('divisor').value);
+    const result = divCalc(x, y);
+    document.getElementById('remainder').textContent = result.value;
+    document.getElementById('remainder-description').textContent = result.description;
+}
---
+++ calc.js
@@ -0,0 +1,42 @@
+'use strict';
+/* exported sumCalc, subCalc, multCalc, divCalc */
+
+// Addition Function
+function sumCalc(x, y) {
+    const sum = x + y;
+
+    return {
+        value: sum,
+        description: 'The sum of ' + x + ' and ' + y + ' is ' + sum
+    };
+}
+
+// Subtraction Function
+function subCalc(x, y) {
+    const sub = x - y;
+
+    return {
+        value: sub,
+        description: 'The difference of ' + x + ' and ' + y + ' is ' + sub
+    };
+}
+
+// Multiplication Function
+function multCalc(x, y) {
+    const mult = x * y;
+
+    return {
+        value: mult,
+        description: 'The product of ' + x + ' and ' + y + ' is ' + mult
+    };
+}
+
+// Division Function
+function divCalc(x, y) {
+    const remainder = x % y;
+
+    return {
+        value: remainder,
+        description: 'The remainder of ' + x + ' divided by ' + y + ' is ' + remainder
+    };
+}
